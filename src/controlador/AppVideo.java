d56d3fa9b6f798eef2b003d9b0e21d43fe80e595
Dani Bermeo <danhibermeo@gmail.com>
2018-11-28 16:54:09 +0100

    Cambio de nombre de la carpeta

--- src/controlador/AppVideo.java
+++ src/controlador/AppVideo.java
@@ -3,5 +3,7 @@ package controlador;
 public class AppVideo {
 		public static void main(String[] args) {
 			System.out.println("funciona o no");
+			System.out.println("cambianos el nombre de la carpeta");
 		}
+		
 }
